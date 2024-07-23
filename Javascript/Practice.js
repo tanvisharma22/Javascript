d058ea3e98902b76dad8dac3f224b74e3984d305
Tanvi Sharma <109289510+tanvisharma22@users.noreply.github.com>
2024-07-23 14:03:37 +0530

    Add files via upload

---
+++ Javascript/Practice.js
@@ -0,0 +1,22 @@
+let a = "Tanvi"
+let b = 6
+console.log(a+b)
+
+console.log(typeof (a+b))
+
+const c = {
+    name: "Tanvi",
+    section: 1,
+    isPrincipal:false
+
+}
+c['friend'] = "Mansi"
+c['name'] = "Amit"
+console.log(c)
+
+const dict = {
+    appreciate: "recognize full worth of",
+    excruciating : "Unbearable",
+    yakka : "hardwork"
+}
+console.log(dict.yakka)
